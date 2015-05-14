a41f2cdb6a428b584ed4269511047a4a51a5eb86
Alexander <alexanno@gmail.com>
2015-05-14 23:03:04 +0200

    debug projection issues. Looks like /eksport/ gives coords in 3857 - but shifted westward. Should abandon csv-approach and use plain NVDB /sok/ endpoint with .json response. Branch should not be merged - but for archiving.

--- www/veiledning01/js/eks01.js
+++ www/veiledning01/js/eks01.js
@@ -28,10 +28,11 @@ window.onload = function(){
       attribution: '&copy; <a href="">...</a>'
   });
   // Lagene tilføres array som tilføres kartet:
-  layers.push(osm, watercoler, toner);
+  //layers.push(osm, watercoler, toner);
+  layers.push(toner);
   var map = app.map = L.map('map', {
       center: [59.91235, 10.7357]
-    , zoom: 11
+    , zoom: 4
     , layers: layers
   })
   //return;
@@ -45,7 +46,7 @@ window.onload = function(){
 
   // var marker = L.marker([59.91118, 10.70297]).addTo(map);
 
-  map.on('click', function (e) {
+  window.map = map.on('click', function (e) {
     log("You clicked the map at " + e.latlng); 
   });
 
@@ -53,8 +54,12 @@ window.onload = function(){
   //omnivore.geojson('data/trafikulykker01.nvdb.json').addTo(map);
 
   // Proj4js.defs["EPSG:32633"] = "+proj=utm +zone=33 +ellps=WGS84 +datum=WGS84 +units=m +no_defs";
-  // proj4.defs('WGS84', "+title=WGS 84 (long/lat) +proj=longlat +ellps=WGS84 +datum=WGS84 +units=degrees");
+  //  proj4.defs("EPSG:4326","+proj=longlat +datum=WGS84 +no_defs");
+  proj4.defs("EPSG:25833","+proj=utm +zone=33 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs");
+  proj4.defs("EPSG:25832","+proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs");
   proj4.defs('EPSG:32633', "+proj=utm +zone=33 +ellps=WGS84 +datum=WGS84 +units=m +no_defs");
+  proj4.defs("EPSG:32632","+proj=utm +zone=32 +datum=WGS84 +units=m +no_defs");
+  proj4.defs("EPSG:3857","+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs");
 
   var baseUrl = window.location.protocol + '//' + window.location.host + '/';
   Papa.parse("../data/trafikulykker02.nvdb.csv", {
@@ -68,12 +73,16 @@ window.onload = function(){
         var srcGeom = wellknown.parse(wkt), dstCoords;
         if(typeof srcGeom === 'object' && srcGeom !== null){
           log("Geometri, punkt:", wkt, srcGeom.coordinates);
-          dstCoords = proj4("EPSG:32633", "EPSG:3857", srcGeom.coordinates);
-          // dstCoords = proj4("EPSG:32633", "EPSG:3857", [srcGeom.coordinates[1], srcGeom.coordinates[0]]);
-          log('dstGeom',dstCoords);
-          L.marker(dstCoords).addTo(map)
-        }
-      }
+          //dstCoords = proj4("EPSG:32633", "EPSG:4326", srcGeom.coordinates);
+          console.log(srcGeom.coordinates);
+          dstCoords = proj4("EPSG:3857", "EPSG:4326", [srcGeom.coordinates[1], srcGeom.coordinates[0]]);
+          console.log(dstCoords);
+          //add to grouplayer
+          L.marker(dstCoords).addTo(map);
+
+          return;
+        }//else?
+      }//else?
     },
     complete: function() {
       log("All done!");
