02c506b9a7b1d3680d67067560ace886b85d50a6
elvtr <mail@ellevator.net>
2015-05-14 18:47:33 +0200

    papaparse reads csv data

--- www/veiledning01/js/eks01.js
+++ www/veiledning01/js/eks01.js
@@ -1,4 +1,9 @@
 window.app = {};
+window.log = function(){
+  if(typeof console !== 'undefined' && typeof console.log === 'function'){
+    console.log(arguments);
+  }
+};
 
 //$(document).ready(function(){
 window.onload = function(){
@@ -39,10 +44,25 @@ window.onload = function(){
   // var marker = L.marker([59.91118, 10.70297]).addTo(map);
 
   map.on('click', function (e) {
-    console.log("You clicked the map at " + e.latlng); 
+    log("You clicked the map at " + e.latlng); 
   });
 
   //omnivore.csv('data/trafikulykker01.nvdb.csv').addTo(map);
   //omnivore.geojson('data/trafikulykker01.nvdb.json').addTo(map);
+
+  var baseUrl = window.location.protocol + '//' + window.location.host + '/';
+  Papa.parse("../data/trafikulykker02.nvdb.csv", {
+    download: true,
+    worker: true,
+    header: true,
+    step: function(row) {
+      log("Row:", row.data);
+      log("Geometri, punkt:", row.data[0]["Geometri, punkt"]);
+      //log("wkt:", row.data[0][32]);
+    },
+    complete: function() {
+      log("All done!");
+    }
+  });
 };
 //});
