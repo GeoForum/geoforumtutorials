1f811b7f65f176d81b50faeb1e5e2a96ba0e3d30
elvtr <mail@ellevator.net>
2015-05-14 19:38:36 +0200

    eks01 problems with proj4 conversion:
    File www/veiledning01/js/eks01.js: Managed to read csv data, fetch the utm 33 wkt string,
    parse it to coordinates using wellknown.js, convert it from 'EPSG:32633' to "EPSG:3857"
    - but the new coordinates have completely far off values.
    
    What goes wrong with conversion?

--- www/veiledning01/js/eks01.js
+++ www/veiledning01/js/eks01.js
@@ -1,3 +1,5 @@
+(function(){ // wrap
+
 window.app = {};
 window.log = function(){
   if(typeof console !== 'undefined' && typeof console.log === 'function'){
@@ -50,15 +52,28 @@ window.onload = function(){
   //omnivore.csv('data/trafikulykker01.nvdb.csv').addTo(map);
   //omnivore.geojson('data/trafikulykker01.nvdb.json').addTo(map);
 
+  // Proj4js.defs["EPSG:32633"] = "+proj=utm +zone=33 +ellps=WGS84 +datum=WGS84 +units=m +no_defs";
+  // proj4.defs('WGS84', "+title=WGS 84 (long/lat) +proj=longlat +ellps=WGS84 +datum=WGS84 +units=degrees");
+  proj4.defs('EPSG:32633', "+proj=utm +zone=33 +ellps=WGS84 +datum=WGS84 +units=m +no_defs");
+
   var baseUrl = window.location.protocol + '//' + window.location.host + '/';
   Papa.parse("../data/trafikulykker02.nvdb.csv", {
     download: true,
     worker: true,
     header: true,
     step: function(row) {
-      log("Row:", row.data);
-      log("Geometri, punkt:", row.data[0]["Geometri, punkt"]);
-      //log("wkt:", row.data[0][32]);
+      //log("Row:", row.data);
+      var wkt = row.data[0]["Geometri, punkt"];
+      if(typeof wkt === 'string'){
+        var srcGeom = wellknown.parse(wkt), dstCoords;
+        if(typeof srcGeom === 'object' && srcGeom !== null){
+          log("Geometri, punkt:", wkt, srcGeom.coordinates);
+          dstCoords = proj4("EPSG:32633", "EPSG:3857", srcGeom.coordinates);
+          // dstCoords = proj4("EPSG:32633", "EPSG:3857", [srcGeom.coordinates[1], srcGeom.coordinates[0]]);
+          log('dstGeom',dstCoords);
+          L.marker(dstCoords).addTo(map)
+        }
+      }
     },
     complete: function() {
       log("All done!");
@@ -66,3 +81,6 @@ window.onload = function(){
   });
 };
 //});
+
+
+})(); // wrap
